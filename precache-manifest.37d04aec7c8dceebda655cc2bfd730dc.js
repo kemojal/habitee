94ba00cc3c028362fb2d26e77fa58a59ed6fd794
kemo <kemo3855@yahoo.com>
2021-04-20 04:36:51 +0800

    Updates

---
+++ precache-manifest.37d04aec7c8dceebda655cc2bfd730dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec08a38b87642b082126396e9194b456",
+    "url": "/habitee/index.html"
+  },
+  {
+    "revision": "c5c75bcd03acab9d98b4",
+    "url": "/habitee/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "81b815ab60892e60a008",
+    "url": "/habitee/static/css/main.3aab5fca.chunk.css"
+  },
+  {
+    "revision": "c5c75bcd03acab9d98b4",
+    "url": "/habitee/static/js/2.1ee152a5.chunk.js"
+  },
+  {
+    "revision": "0c200da8c2578d7778fe06f3ab474d11",
+    "url": "/habitee/static/js/2.1ee152a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b815ab60892e60a008",
+    "url": "/habitee/static/js/main.a44c9942.chunk.js"
+  },
+  {
+    "revision": "3ace7fcd1005e62ecc3a",
+    "url": "/habitee/static/js/runtime-main.22dd634e.js"
+  },
+  {
+    "revision": "d422ad4c81827bf0201dff5c35cdf2fd",
+    "url": "/habitee/static/media/5ec78d543fc8c0012d4aaa3a_1-min.d422ad4c.jpeg"
+  },
+  {
+    "revision": "148cdd9b239f013d389801c208f82a94",
+    "url": "/habitee/static/media/clipshapes.148cdd9b.png"
+  }
+]);
